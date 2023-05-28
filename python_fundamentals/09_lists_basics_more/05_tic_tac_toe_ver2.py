85337fac88a132dfb6578401971b6b8550154eab
Yavor Gornalov <y.gornalov@gmail.com>
2023-05-28 17:56:45 +0300

    05_tic_tac_toe_ver2.py added solved with functions

--- python_fundamentals/09_lists_basics_more/05_tic_tac_toe_ver2.py
+++ python_fundamentals/09_lists_basics_more/05_tic_tac_toe_ver2.py
@@ -1,62 +1,50 @@
 # https://judge.softuni.org/Contests/Practice/Index/1726#4
 
 
-def row_winner():
-    for player in players:
+def row_winner(symbol):
+    for row in range(size):
+        if all(x == symbol for x in playground[row]):
+            return 1
+    return 0
+
+
+def column_winner(symbol):
+    for col in range(size):
+        column = []
         for row in range(size):
-            if all(x == player for x in playground[row]):
-                return player
+            column.append(playground[row][col])
+        if all(x == symbol for x in column):
+            return 1
     return 0
 
 
-def column_winner():
-    for player in players:
-        for col in range(size):
-            column = []
-            for row in range(size):
-                column.append(playground[row][col])
-
-            if all(x == player for x in column):
-                return player
-    return 0
-
-
-def diagonal_winner():
+def diagonal_winner(symbol):
     left_diagonal, right_diagonal = [], []
-
     for i in range(size):
         left_diagonal.append(playground[i][i])
         right_diagonal.append(playground[i][size - i - 1])
 
-    for player in players:
-        if all(x == player for x in left_diagonal) or all(x == player for x in right_diagonal):
-            return player
+    if all(x == symbol for x in left_diagonal) or all(x == symbol for x in right_diagonal):
+        return 1
     return 0
 
 
 def gameplay():
-    winner = row_winner()
-    if winner:
-        return winner
-
-    winner = column_winner()
-    if winner:
-        return winner
-
-    winner = diagonal_winner()
-    if winner:
-        return winner
+    for player in players:
+        if row_winner(player) or column_winner(player) or diagonal_winner(player):
+            return player
+    return 0
 
 
-players = [1, 2]
+players = ["1", "2"]
 size = 3
-playground = [[int(x) for x in input().split()] for _ in range(size)]
+playground = [input().split() for _ in range(size)]
 
-game_winner = gameplay()
+winner = gameplay()
 
-if game_winner == players[0]:
+if winner == players[0]:
     print("First player won")
-elif game_winner == players[1]:
+elif winner == players[1]:
     print("Second player won")
 else:
     print("Draw!")
